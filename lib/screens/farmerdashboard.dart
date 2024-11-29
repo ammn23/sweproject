7dc6c348692eb3a8655674b4806329ded4ff656b
Amina <aimuratovaamina34@gmai.com>
2024-11-30 00:44:17 +0500

    changes to farmer

--- lib/screens/farmerdashboard.dart
+++ lib/screens/farmerdashboard.dart
@@ -1,9 +1,11 @@
+import 'package:farmersmarketflutter/screens/farmershowpl.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert'; // For JSON decoding
 import 'package:http/http.dart' as http; // For making REST API calls
 import 'farminfo.dart';
 import 'farmerinfo.dart';
+import 'farmereditpl.dart';
 
 class FarmerDashboard extends StatefulWidget {
   const FarmerDashboard({super.key});
@@ -101,6 +103,18 @@ class _FarmerDashboardState extends State<FarmerDashboard> {
                       Text('Welcome, $name!',
                           style: const TextStyle(fontSize: 20)),
                       const SizedBox(height: 20),
+
+
+                      ElevatedButton(
+                        onPressed: () {
+                          // Navigate to the NewPage
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => (ProductListPage(userId: userId!))),
+                          );
+                        },
+                        child: const Text('Go to New Page'),
+                      ),
 
                       // Heading for Farmer Info
                       const Text(
